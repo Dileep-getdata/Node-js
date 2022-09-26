e4c7ddf9e625d53d80578093609c0aff8112a9b8
Dileep Thiriveedhi <dileep2424@gmail.com>
2022-09-26 10:36:37 +0530

    request and response

--- first-app.js
+++ first-app.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+const http=require(`http`);
+const server=http.createServer((req,res)=>{
+    console.log(req.url,req.method,req.headers);
+// process.exit()
+    res.setHeader('Content-type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My first page</title></head>');
+    res.write('<body><h1>Welcome to my node JS project</h1></body>');
+    res.end();
+});
+server.listen(3000)
