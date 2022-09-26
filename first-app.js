cecad77e724244e54846f16434350ee60d6431d6
Dileep Thiriveedhi <dileep2424@gmail.com>
2022-09-26 20:00:34 +0530

    input to file and file to html (reading and writing file)

--- first-app.js
+++ first-app.js
@@ -1,36 +1,57 @@
 const http=require('http');
 const fs=require('fs');
-const { buffer } = require('stream/consumers');
 
-const server=http.createServer((req,res)=>{
-const url=req.url;
-const method=req.method;
-if(url==='/'){
-    res.write('<html>');
-    res.write('<head><title>My message page</title></head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-    res.write('</html>');
-   return  res.end();
-}
 
-if(url==='/message' && method==='POST'){
-    const body=[];
-    req.on('data',(chunk)=>{
-        // console.log(chunk);
-        body.push(chunk);        
 
+const server=http.createServer((req,res)=>
+{
+    const url=req.url;
+    const method=req.method;
+
+    if(url==='/')
+    {        
+        fs.readFile('message.txt',{encoding:"utf-8"},(err,data)=>{
+            if(err){
+                console.log(err);
+            }        
+        res.write('<html>');
+        res.write('<head><title>My message page</title></head>');
+        res.write(`<body><p id="fromFile">${data}</p></body>`)
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');        
+        return  res.end();
     });
-    req.on('end',()=>{
-        const parseBody=Buffer.concat(body).toString();
-        const message=parseBody.split('=')[1];
-        fs.writeFileSync('message.txt',message);
+    }
 
-    });
-    // fs.writeFileSync('message.txt','dum');
-    res.statusCode=302;
-    res.setHeader('Location','/');
-    return res.end();
-}
+    if(url==='/message' && method==='POST')
+    {
+        const body=[];
+        req.on('data',(chunk)=>
+        {
+            // console.log(chunk);
+            body.push(chunk);        
+
+        });
+        return req.on('end',()=>
+        {
+                const parseBody=Buffer.concat(body).toString();
+                const message=parseBody.split('=')[1];
+                fs.writeFile('message.txt',message,err=>
+                {
+                    if(err){
+                        console.log(err);
+                    }
+                    res.statusCode=302;
+                    res.setHeader('Location','/');            
+                    return res.end();
+                
+                });
+                
+        });       
+
+        
+    
+    }
 
 });
 server.listen(4000);
