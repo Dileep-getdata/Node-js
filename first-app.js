e83c7943cfd6364803c9c9339a77e4998519af31
Dileep Thiriveedhi <dileep2424@gmail.com>
2022-10-12 22:21:59 +0530

    Body parser response.send using path's

--- first-app.js
+++ first-app.js
@@ -3,15 +3,23 @@ const http=require('http');
 // Importing the exported file
 const routes=require('./routes');
 const express=require('express');
+// const bodyparser=require('body-parser');
+const { urlencoded } = require('body-parser');
 const app=express();
 
-app.use((req,res,next)=>{
-    console.log('In the middleware');
-    next();
+app.use(urlencoded({extended: false}));
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="title1"><button type="submit">Add product</button></form>')
 });
 
-app.use((req,res,next)=>{
-    res.send('<h1> Hello Express JS</h1>');
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/',(req,res,next)=>{
+    res.send('<h1>Hello from express JS!</h1>')
 });
 
 // const server=http.createServer(app); 
