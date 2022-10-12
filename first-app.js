d185c6d66e9562d321d31e17b137ffb303190c89
Dileep Thiriveedhi <dileep2424@gmail.com>
2022-10-12 09:21:48 +0530

    Express JS used next function and middleware

--- first-app.js
+++ first-app.js
@@ -2,8 +2,19 @@ const http=require('http');
 
 // Importing the exported file
 const routes=require('./routes');
+const express=require('express');
+const app=express();
 
-const server=http.createServer(routes.handler); 
-console.log(routes.text);
+app.use((req,res,next)=>{
+    console.log('In the middleware');
+    next();
+});
 
-server.listen(4000);
+app.use((req,res,next)=>{
+    res.send('<h1> Hello Express JS</h1>');
+});
+
+// const server=http.createServer(app); 
+// // console.log(routes.text);
+
+app.listen(4000);
